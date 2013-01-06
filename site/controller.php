7b359c9bb555460529760b8103facca12d85aaf9
Robert Skolnick <gator@brazitech.com>
2013-01-06 11:02:18 -0200

    update site controller

---
+++ site/controller.php
@@ -0,0 +1,13 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+ 
+/**
+ * BrazitTrac Component Controller
+ */
+class BraziTracController extends JController
+{
+}
